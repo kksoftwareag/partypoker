96cd02e77a82b6b3b1bf8b1efc0c83b642647516
Leiber, Thomas <Leiber@kk-software.de>
2020-10-30 00:30:34 +0000

    * added css live-reloading
    git-tfs-id: [https://tfs.kk-software.de/DefaultCollection]$/KK/Products/PlanningPoker;C12763

--- PlanningPoker.Web/Startup.cs
+++ PlanningPoker.Web/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Toolbelt.Extensions.DependencyInjection;
 
 namespace PlanningPoker.Web
 {
@@ -28,6 +29,7 @@ namespace PlanningPoker.Web
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseCssLiveReload();
             }
             else
             {
