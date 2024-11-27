19b653c13019cdfda1237a4c01afb3f026d82029
Thomas Leiber <leiber@kk-software.de>
2024-11-27 18:18:17 +0100

    undo class refactor

--- PlanningPoker.Web/wwwroot/js/audio.js
+++ PlanningPoker.Web/wwwroot/js/audio.js
@@ -1,12 +1,10 @@
-﻿class MediaPlayer
-{
-    static PlayAudio(filename)
-    {
-        let mp3 = '/audio/' + filename + '.mp3';
-        let wav = '/audio/' + filename + '.wav';
+﻿var MediaPlayer = {
+    PlayAudio: function (filename) {
+        const mp3 = '/audio/' + filename + '.mp3';
+        const wav = '/audio/' + filename + '.wav';
 
         let audio = new Audio();
         audio.src = audio.canPlayType('audio/mpeg') ? mp3 : wav;
         audio.play();
     }
-}
+};
